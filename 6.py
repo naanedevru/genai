5771bd69157ccfae2871f4bc1c12566d5e6ee774
jaishreeRam <116339039+naanedevru@users.noreply.github.com>
2025-06-11 21:09:53 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,16 +1,11 @@
-!pip -q install accelerate -U
-!pip -q install transformers[torch]
-!pip -q install datasets
-
+PG 6
+!pip install -q accelerate transformers[torch] datasets
 import pandas as pd
 from transformers import pipeline
-
 senti_model = pipeline("sentiment-analysis")
-
 print(senti_model("This movie is damn good. I loved it"))
 print(senti_model("This is a bad phone. The screen and battery are of poor quality."))
 print(senti_model("Over heating issue don't by this product camera was good"))
-
 df = pd.read_csv("https://raw.githubusercontent.com/venkatareddykonasani/Datasets/master/Amazon_Yelp_Reviews/Review_Data.csv").sample(50)
 df["Predicted_Sentiment"] = df["Review"].apply(lambda x: senti_model(x)[0]["label"])
 df
