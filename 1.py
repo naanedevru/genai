c962faf7868a439219b22df19c5d26e34696a7b2
jaishreeRam <116339039+naanedevru@users.noreply.github.com>
2025-06-11 21:07:52 +0530

    Update 1

--- 1.py
+++ 1.py
@@ -1,11 +1,8 @@
-
+PG 1
 import gensim.downloader as api
-
 model = api.load("word2vec-google-news-300")
-
 queen_vector = model.get_vector("king") - model.get_vector("man") + model.get_vector("woman")
 print(model.similar_by_vector(queen_vector, topn=1))
-
 if "actor" in model.key_to_index:
     actor_vector = model.get_vector("actor") - model.get_vector("man") + model.get_vector("woman")
     print(model.similar_by_vector(actor_vector, topn=5))
